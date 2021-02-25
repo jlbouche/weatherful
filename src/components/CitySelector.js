59f3a5306fa48d5fc6bc777385737bb4e4ac4e68
JL <jennalbouche@gmail.com>
2021-02-25 12:08:54 -0800

    update cityselector structure, update app.js for cityselector, minor css

--- src/components/CitySelector.js
+++ src/components/CitySelector.js
@@ -1,8 +1,29 @@
 import React, {useState} from 'react';
+import {Row, Col, FormControl, Button} from 'react-bootstrap';
 
 export default function CitySelector(){
     const [city, setCity] = useState('');
     return (
-        <div>CITY</div>
+        <>
+            <Row>
+                <Col>
+                <h1>Search your city:</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={4} className="text-center">
+                    <FormControl
+                        placeholder="Enter city name"
+                        onChange={(event) => setCity(event.target.value)}
+                        value={city}
+                    />
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                <Button onClick={onSearch}>Check Weather</Button>
+                </Col>
+            </Row>
+        </>
     )
 }
